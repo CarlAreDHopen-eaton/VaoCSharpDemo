8a3b1d2ee96c33d09b4652a142b1d096ab8742d5
Carl Are Dreier Hopen <CarlAreDHopen@eaton.com>
2022-08-02 15:22:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace VaoCSharpDemo
+{
+   class Program
+   {
+      private static bool UseHttps = false;
+      private static string Password = "SomePassword";
+      private static string User = "SomeUsername";
+      private static string Address = "SomeAddress";
+      private static string Port = "444";
+
+      static void Main(string[] args)
+      {
+         RestResponse response = GetVaoStatus();
+         Console.WriteLine(response.Content);
+         Console.ReadKey();
+      }
+
+      private static RestResponse GetVaoStatus()
+      {
+         var client = GetRestClient();
+         RestRequest request = new RestRequest("status", Method.Options);
+         RestResponse response = client.Execute(request);
+         return response;
+      }
+
+      private static RestClient GetRestClient()
+      {
+         string addressLine = string.Format(@"{0}://{1}:{2}", UseHttps ? "https" : "http", Address, Port);
+         RestClient client = new RestClient(addressLine);
+         client.Authenticator = new HttpBasicAuthenticator(User, Password);
+         return client;
+      }
+   }
+}
