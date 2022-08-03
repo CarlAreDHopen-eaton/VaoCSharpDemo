b92dd4a585a17a23ea0352bdfcf067b47307bc1d
Carl Are Dreier Hopen <CarlAreDHopen@eaton.com>
2022-08-03 16:37:45 +0200

    Added support for HTTPS.

--- Program.cs
+++ Program.cs
@@ -17,11 +17,29 @@ namespace VaoCSharpDemo
          // Query the VMS system.
          Console.WriteLine($"Checking VMS status (Host:{mProgramOptions.Host}:{mProgramOptions.Port} Secure:{mProgramOptions.UseHttps}).");
          RestResponse response = GetVaoStatus();
-         Console.WriteLine(response.Content);
+
+         // Write the response or error to the console.
+         WriteResponse(response);
 
          // Allow user to read output
          Console.WriteLine("Press any key to continue.");
          Console.ReadKey();
+      }
+
+      private static void WriteResponse(RestResponse response)
+      {
+         if (response.IsSuccessful)
+         {
+            Console.WriteLine(response.Content);
+         }
+         else
+         {
+            Console.WriteLine($"Request failed, {response.ErrorException}");
+            if (response.ErrorException?.InnerException != null)
+            {
+               Console.WriteLine(response.ErrorException.InnerException);
+            }
+         }
       }
 
       private static RestResponse GetVaoStatus()
@@ -34,9 +52,22 @@ namespace VaoCSharpDemo
 
       private static RestClient GetRestClient()
       {
+         RestClient client;
          string addressLine = string.Format(@"{0}://{1}:{2}", mProgramOptions.UseHttps ? "https" : "http", mProgramOptions.Host, mProgramOptions.Port);
-         RestClient client = new RestClient(addressLine);
-      
+         if (mProgramOptions.UseHttps && mProgramOptions.IgnoreCertificateErrors)
+         {
+            // Bypass ssl validation check.
+            var options = new RestClientOptions(addressLine)
+            {
+               RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+            };
+            client = new RestClient(options);
+         }
+         else
+         {
+            client = new RestClient(addressLine); 
+         }
+
          client.Authenticator = new HttpBasicAuthenticator(mProgramOptions.User, mProgramOptions.Password);
          return client;
       }
--- ProgramOptions.cs
+++ ProgramOptions.cs
@@ -16,7 +16,11 @@ namespace VaoCSharpDemo
       [Option('h', "host", Required = true, HelpText = "Host/IP to connect to.")]
       public string Host { get; set; }
 
-      [Option('t', "port", Required = false, HelpText = "Port to connect to (Default port 444).")]
+      [Option('t', "port", Required = false, HelpText = "Port to connect to (Default port: 444).")]
       public int Port { get; set; } = 444;
+
+      [Option('i', "ignoreCert", Required = false, HelpText = "Ignore certificate errors (Default: false).")]
+      public bool IgnoreCertificateErrors { get; set; } = false;
+
    }
 }
